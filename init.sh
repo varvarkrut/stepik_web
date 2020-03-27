e45498da87bb513127dc0a5989e3844d4e9ce6a4
varvarkrut <vanya.shcurko@gmail.com>
2020-03-27 22:50:56 +0300

    fixed config's and added gunicorn's settings in init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -s ./etc/hello.py /etc/gunicorn.d/hello.py
