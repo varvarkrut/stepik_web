8c5ccc1dbd7e054949a408661e91e8f10d1dcbfc
varvarkrut <vanya.shcurko@gmail.com>
2020-04-01 22:12:22 +0300

    django ask and qa

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s home/box/etc/hello.py /etc/gunicorn.d/hello.py
+sudo gunicorn -c /home/box/web/etc/hello.py hello:wsgi_application
+sudo gunicorn -c /home/box/web/etc/ask.conf.py ask.wsgi:application
