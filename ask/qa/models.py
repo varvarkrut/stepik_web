c2f64f19ca746e42d20f15e26133989f0028f3da
e1_name <vanya.shcurko@gmail.com>
2020-04-14 21:18:30 +0300

    added views

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class QuestionManager(models.Manager):
-    def popular(self):
-        return (self.objects.order_by('rating'))
-    def new(self):
-        return (self.objects.order_by(added_at.desc()))
+    @staticmethod
+    def new():
+        return Question.objects.all().order_by('-pk')
+
+    @staticmethod
+    def popular():
+        return Question.objects.all().order_by('-rating')
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
@@ -15,6 +19,9 @@ class Question(models.Model):
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes =  models.ManyToManyField(User, related_name='question_like_user')
     objects = QuestionManager()
+
+
+
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,3 +1,35 @@
-from django.http import HttpResponse 
-def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+from django.http import Http404
+from django.shortcuts import render
+from .models import Question
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_GET
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+def paginate(request, qs):
+    try:
+        limit = int(request.GET.get('limit', 10))
+    except ValueError:
+        limit = 10
+    if limit > 100:
+        limit = 10
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        raise Http404
+    paginator = Paginator(qs, limit)
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return page
+
+
+@require_GET
+def new(request):
+    questions = Question.objects.new()
+    page = paginate(request, questions)
+    page.baseurl = '/?page='
+    return render(request, 'index.html', {
+        'questions':  page.object_list,
+        'page':       page,
+    })
