f1bbcaf31ea239b0024cb74d372e1835f66d4e93
e1_name <vanya.shcurko@gmail.com>
2020-04-15 23:23:01 +0300

    added valid index.html

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ch!*2e%el+e*d(wbk1rm3_rp-tb0^)st2o&gsfkh561)aoag+k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap3',
     'qa',
 ]
 
